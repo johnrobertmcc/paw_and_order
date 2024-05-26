db95f452fd5b643b7e2bc649d334c6ca6265406d
JR <john.robert.mcc@gmail.com>
2024-05-26 18:05:39 -0400

    slice

--- src/pages/donate/index.js
+++ src/pages/donate/index.js
@@ -1,5 +1,13 @@
 import Container from "@/components/Container";
 
 export default function Donate(){
-    return <Container tag="article">Donation</Container>
+    return <Container tag="article">
+        <section>
+            <h1>
+                Donating to Paw & Order
+            </h1>
+            <p>Unfortunately, as a 12 year old, Cato is unable to open his own bank account until he is 13.</p>
+            <p>Until then, please <a href="mailto:pawnorder@proton.me">email me</a> or <a href="tel:9295608958">call me</a> and let me help you donate to me directly.</p>
+        </section>
+    </Container>
 }
